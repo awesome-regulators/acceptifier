5a1b09ac39b78e31f75218a66806d639c56006a3
Clayton <clayton.lengel-zigich@pearson.com>
2012-10-29 17:35:35 -0700

    Base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acceptifier::Application.config.secret_token = '9509c16276d58d6dabc2df551ac845581943e0b1d075fefda8b62a5d833854951b2d0fcca19efbcb0dce2f3d7ca5317b1b679f04f680d56582d09aa93dce7d3f'
